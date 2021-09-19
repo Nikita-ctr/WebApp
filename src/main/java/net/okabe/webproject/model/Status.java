af5ce238e78685a9f5a332c5f576d3c2dc5c3638
Nikita <xmil.nik@gmail.com>
2021-09-20 00:55:32 +0300

    Initial commit

---
+++ src/main/java/net/okabe/webproject/model/Status.java
@@ -0,0 +1,5 @@
+package net.okabe.webproject.model;
+
+public enum Status {
+    TRUE,FALSE
+}
---
+++ src/main/java/net/okabe/webproject/security/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package net.okabe.webproject.security;
+
+import net.okabe.webproject.model.User;
+import net.okabe.webproject.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service("userDetailsServiceImpl")
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user=userRepository.findByEmail(email).orElseThrow(()->new UsernameNotFoundException("User doesnt exist"));
+        return SecurityUser.fromUser(user);
+    }
+}
